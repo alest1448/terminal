fae4b21dabe5591704ddd1d79a7865c77d958e68
alest1448 <118421044+alest1448@users.noreply.github.com>
2023-06-29 02:28:27 +0900

    Initial commit

---
+++ terminal/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace terminal
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        public int AddNumber(string number)
+        {
+            
+            var check = -1;
+                MySqlConnection conn = new MySqlConnection("server=localhost; user id = root; password=z213461z; database = ochered"); //строка подключениz
+                conn.Open();
+                string cndText = $"INSERT INTO Spisok(Number) VALUES('{number}' );"; 
+                MySqlCommand cmd = new MySqlCommand(cndText, conn);
+                check = cmd.ExecuteNonQuery();
+
+                conn.Close();
+                return check;
+            }
+            private void btPoluch_Click(object sender, RoutedEventArgs e)
+        {
+
+            string iPass = "";
+            string[] arrb = { "А", "Б", "В", "Г", "Д", "Е", "З", "Ж", "И", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т" };
+            string[] arr = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i = i + 1)
+            {
+                iPass = iPass + arr[rnd.Next(0, 9)];
+
+            }
+            for (int j = 0; j < 1; j = j + 1) //генерация номера
+            {
+
+                iPass = arrb[rnd.Next(0, 18)] + "-" + iPass;
+            }
+
+            if (AddNumber(iPass) > 0) //добавление номера в список
+            {
+                Pecat window1 = new Pecat();
+                window1.Show();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Не удалось завершить действие");
+
+            }
+
+        }
+    }
+}
---
+++ terminal/Pecat.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml.Linq;
+using System.Speech.Synthesis;
+using System.Drawing.Printing;
+
+namespace terminal
+{
+    /// <summary>
+    /// Логика взаимодействия для Pecat.xaml
+    /// </summary>
+    public partial class Pecat : Window
+    {
+      
+                 public Pecat()
+        {
+            InitializeComponent();
+
+            MySqlConnection conn = new MySqlConnection("server=localhost; user id = root; password=z213461z; database = ochered"); //строка подключениz
+            string sql = "SELECT number FROM spisok ORDER BY idSpisok DESC LIMIT 1";
+
+
+
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+            {
+                DataTable dt = new DataTable();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+                lbNum.Content = cmd.ExecuteScalar();
+                SpeechSynthesizer speechSynth = new SpeechSynthesizer(); // создаём объект
+                speechSynth.Volume =100; // устанавливаем уровень звука
+                speechSynth.Speak($"Ваш номер {lbNum.Content}"); // озвучиваем переданный текст
+                conn.Close();
+            }
+         
+           
+
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(10) }; //переход обратно спустя 10 секунд
+            timer.Start();
+            timer.Tick += (sender, args) =>
+            {
+                timer.Stop();
+                var page = new MainWindow();
+                page.Show();
+                this.Close();
+            };
+        }
+    }
+}
+
